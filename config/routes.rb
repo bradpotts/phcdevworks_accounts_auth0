ee5231cad1560eb9d72303393a7e04e2bb5ca85b
Brad Potts <brad.potts@phcnet.co>
2022-09-10 07:29:12 +0800

    adjustments to make it work

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ PhcdevworksAccountsAuth0::Engine.routes.draw do
     match "user/profile", to: "user/pages#profile", via: "get"
 
     # Auth Routes
-    get '/auth/auth0/callback' => 'auth/handler#callback'
-    get '/auth/failure' => 'auth/handler#failure'
-    get '/auth/logout' => 'auth/handler#logout'
-    get '/auth/redirect' => 'auth/handler#redirect'
+    get "/auth/auth0", as: "authentication"
+    get "/auth/auth0/callback" => "auth/handler#callback"
+    get "/auth/failure" => "auth/handler#failure"
+    get "/auth/logout" => "auth/handler#logout"
+    get "/auth/redirect" => "auth/handler#redirect"
 
 end
