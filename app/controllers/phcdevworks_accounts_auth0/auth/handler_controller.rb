6f8db8a39cd93e609c75cf308a3583b9f0d20e71
Brad Potts <brad.potts@phcnet.co>
2022-09-11 10:20:10 +0800

    Top/Side Menu

--- app/controllers/phcdevworks_accounts_auth0/auth/handler_controller.rb
+++ app/controllers/phcdevworks_accounts_auth0/auth/handler_controller.rb
@@ -2,13 +2,13 @@ module PhcdevworksAccountsAuth0
   class Auth::HandlerController < ApplicationController
 
     def callback
-      auth_info = request.env['omniauth.auth']
-      session[:userinfo] = auth_info['extra']['raw_info']
-      redirect_to '/user'
+      auth_info = request.env["omniauth.auth"]
+      session[:userinfo] = auth_info["extra"]["raw_info"]
+      redirect_to "/"
     end
 
     def failure
-      @error_msg = request.params['message']
+      @error_msg = request.params["message"]
     end
 
     def logout
@@ -22,14 +22,14 @@ module PhcdevworksAccountsAuth0
 
     def logout_url
       request_params = {
-        returnTo: root_url,
-        client_id: AUTH0_CONFIG['auth0_client_id']
+        returnTo: main_app.root_path,
+        client_id: AUTH0_CONFIG["auth0_client_id"]
       }
-      URI::HTTPS.build(host: AUTH0_CONFIG['auth0_domain'], path: '/v2/logout', query: to_query(request_params)).to_s
+      URI::HTTPS.build(host: AUTH0_CONFIG["auth0_domain"], path: "/v2/logout", query: to_query(request_params)).to_s
     end
 
     def to_query(hash)
-      hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join('&')
+      hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join("&")
     end
 
   end
