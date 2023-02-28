b4b401b993da9f33f5d38161c373fe9935836529
Brad Potts <brad.potts@phcnet.co>
2023-02-28 10:54:20 +0800

    removed local config, add engine dependencies, cleaned up auth0

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,14 +1,14 @@
 AUTH0_CONFIG = Rails.application.config_for(:auth0)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
-    :auth0,
-    AUTH0_CONFIG['auth0_client_id'],
-    AUTH0_CONFIG['auth0_client_secret'],
-    AUTH0_CONFIG['auth0_domain'],
-    callback_path: '/auth/callback',
-    authorize_params: {
-      scope: 'openid profile'
-    }
-  )
+    provider(
+        :auth0,
+        AUTH0_CONFIG['auth0_client_id'],
+        AUTH0_CONFIG['auth0_client_secret'],
+        AUTH0_CONFIG['auth0_domain'],
+        callback_path: '/auth/callback',
+        authorize_params: {
+            scope: 'openid profile'
+        }
+    )
 end
--- lib/phcdevworks_accounts_auth0/engine.rb
+++ lib/phcdevworks_accounts_auth0/engine.rb
@@ -1,5 +1,19 @@
 module PhcdevworksAccountsAuth0
-  class Engine < ::Rails::Engine
-    isolate_namespace PhcdevworksAccountsAuth0
-  end
+    class Engine < ::Rails::Engine
+
+        # PHCDEVONE - Add Requried Dependencies
+        require "omniauth-auth0"
+        require "omniauth-rails_csrf_protection"
+
+        # PHCDEVONE - Isolate Engine
+        isolate_namespace PhcdevworksAccountsAuth0
+
+        # PHCDEVONE - Rspec Generators
+        config.generators do |g|
+            g.test_framework :rspec
+            g.fixture_replacement :factory_bot
+            g.factory_bot dir: 'spec/factories'
+        end
+
+    end
 end
